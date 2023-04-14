219e9be32d701c1244ddfbc3cbd1592550ce9f9b
Peter Tran <xuanbachtran02@gmail.com>
2023-04-14 02:34:29 -0700

    solved with dp

--- CSES/Maximum_Subarray_Sum.cpp
+++ CSES/Maximum_Subarray_Sum.cpp
@@ -7,21 +7,19 @@ int main()
     ios::sync_with_stdio(false); cin.tie(nullptr);
 
     int n; cin >> n;
-    vector<ll> arr(n);
+    ll res = -1;
+    vector<ll> arr(n), dp(n);
     
     for (auto &i : arr)
         cin >> i;
 
-    ll res = 0;
-    for (int i = 0 ; i < n; i++){
-        if (res + arr[i] < 0){
-            res = 0;
-            continue;
-        }
-
-        
-            
+    dp[0] = arr[0];
+    res = arr[0];
+    for (int i = 1; i < n; i++){
+        dp[i] = max(arr[i], dp[i - 1] + arr[i]);
+        res = max(dp[i], res);
     }
 
+    cout << res << endl;
     return 0;
 }
