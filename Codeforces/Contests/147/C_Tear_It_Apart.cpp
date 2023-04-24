a859b8fbce1174f9198d1c3803f95fc47635304f
Peter Tran <xuanbachtran02@gmail.com>
2023-04-23 22:30:49 -0700

    solved C of last cf contest

--- Codeforces/Contests/147/C_Tear_It_Apart.cpp
+++ Codeforces/Contests/147/C_Tear_It_Apart.cpp
@@ -8,9 +8,30 @@ int main()
     while (TC--){
         string str; cin >> str;
         int n = str.size();
+        int res = 1e6;
+        set<char> s;
 
-        
+        for (char i : str)
+            s.insert(i);
+
+        for (char i : s){
+            int max_a = -1, cnt = 0;
+            for (char j : str){
+                if (j != i){
+                    cnt++;
+                    max_a = max(max_a, cnt);
+                }
+                else
+                    cnt = 0;
+            }
+            int tmp = 0;
+            while (max_a > 0){
+                tmp++;
+                max_a /= 2;
+            }
+            res = min(res, tmp);
+        }
+        cout << res << endl;
     }
-
     return 0;
 }
